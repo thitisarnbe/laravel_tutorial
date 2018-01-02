40dd667096081de4a6d1ea9c457cb0b31ce554bf
Thitisarnbee Chuechun <thitisarnbee@gmail.com>
2018-01-02 10:46:49 +0700

    admin master page setup

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@
 */
 
 Route::get('/', function () {
+   
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin',function(){
+   
+   return view('admin.index'); 
+});
+
+Route::resource('admin/users','AdminUsersController');
